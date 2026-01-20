1c589720c28292230453e1bfa77f9beea67d6cac
Muyleang Ing <116934056+MuyleangIng@users.noreply.github.com>
2026-01-20 17:35:37 +0900

    Update Quantum Clipboard to version 0.1.2

--- Casks/quantum-clipboard.rb
+++ Casks/quantum-clipboard.rb
@@ -2,7 +2,7 @@ cask "quantum-clipboard" do
   version "0.1.2"
   sha256 "26811d6768d7d1dca1b748d9b1ff4f7db980185b2eaef8a09e03c53b45ffe18c"
 
-  url "https://github.com/MuyleangIng/Quantum-Clipboard-Copy/releases/download/0.1.0/Quantum-Clipboard.app.zip"
+  url "https://github.com/MuyleangIng/Quantum-Clipboard-Copy/releases/download/0.1.2/Quantum-Clipboard.app.zip"
   name "Quantum Clipboard"
   desc "Open-source clipboard manager for macOS"
   homepage "https://github.com/MuyleangIng/Quantum-Clipboard-Copy"
